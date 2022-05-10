59085b160d01470292e76e759a0cd0f06632e71c
N. Ry Simmons-Davis <88639333+rysimmonsdavis@users.noreply.github.com>
2022-05-09 23:03:20 -0700

    Roman_to_Integer and Integer_to_Roman files committed

---
+++ 12.Integer_to_Roman.js
@@ -0,0 +1,78 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+
+//Given an integer, convert it to a roman numeral.
+
+ var intToRoman = function(num) {
+    let string = "";
+    
+    while (num - 1000 >= 0){
+        num -= 1000;
+        string += "M";
+    };
+    
+    while (num - 900 >= 0){
+        num -= 900;
+        string += "CM";
+    };
+    
+    while (num - 500 >= 0){
+        num -= 500;
+        string += "D";
+    };
+    
+    while (num - 400 >= 0){
+        num -= 400;
+        string += "CD";
+    };
+    
+    while (num - 100 >= 0){
+        num -= 100;
+        string += "C";
+    };
+    
+    while (num - 90 >= 0){
+        num -= 90;
+        string += "XC";
+    };
+    
+    while (num - 50 >= 0){
+        num -= 50;
+        string += "L";
+    };
+    
+    while (num - 40 >= 0){
+        num -= 40;
+        string += "XL";
+    };
+    
+    while (num - 10 >= 0){
+        num -= 10;
+        string += "X";
+    };
+    
+    while (num - 9 >= 0){
+        num -= 9;
+        string += "IX";
+    };
+    
+    while (num - 5 >= 0){
+        num -= 5;
+        string += "V";
+    };
+    
+    
+    while (num - 4 >= 0){
+        num -= 4;
+        string += "IV";
+    };
+    
+    while (num - 1 >= 0){
+        num -= 1;
+        string += "I";
+    };
+    
+    return string;
+};
---
+++ 13.Roman_to_Integer.js
@@ -0,0 +1,65 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+//Given a roman numeral, convert it to an integer.
+
+
+ var romanToInt = function(s) {
+    let number = 0;
+    
+    for (let i = 0; i < s.length; i++){
+        switch(s[i]){
+            case 'I': 
+                if (s[i+1] == 'V'){
+                    number += 4;
+                    i++;
+                    break;
+                } else if (s[i+1] == 'X') {
+                    number += 9;
+                    i++;
+                    break;
+                } else {
+                    number += 1; 
+                    break;
+                }
+            case 'V': number += 5; 
+                break;
+            case 'X': 
+                if (s[i+1] == 'L'){
+                    number += 40;
+                    i++;
+                    break;
+                } else if (s[i+1] == 'C') {
+                    number += 90;
+                    i++;
+                    break;
+                } else { 
+                    number += 10; 
+                    break;
+                }
+            case 'L': number += 50; 
+                break;
+            case 'C': 
+                if (s[i+1] == 'D'){
+                    number += 400;
+                    i++;
+                    break;
+                } else if (s[i+1] == 'M') {
+                    number += 900;
+                    i++;
+                    break;
+                } else {
+                    number += 100; 
+                    break;
+                }
+            case 'D': number += 500; 
+                break;
+            case 'M': number += 1000; 
+                break;
+        };
+    };
+    
+    return number;
+};
