ce069b9597fe599a24edd7ec05968533a512f1ed
N. Ry Simmons-Davis <88639333+rysimmonsdavis@users.noreply.github.com>
2023-05-23 21:51:23 -0700

    p 27

---
+++ Problems/Easy/JavaScript/27. Remove Element.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+
+    let k = nums.length;
+
+    for (let i = 0; i < nums.length; i++){
+
+        if (nums[i] == val) {
+            nums.splice(i,1);
+            i -= 1;
+            k -= 1;
+        }
+    }
+    
+    return k;
+
+};
