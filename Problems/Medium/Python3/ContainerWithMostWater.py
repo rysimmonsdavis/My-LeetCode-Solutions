52102c7f8306df73db7967592ed44c996fbe17ab
N. Ry Simmons-Davis <88639333+rysimmonsdavis@users.noreply.github.com>
2024-02-17 19:04:27 -0800

    latest push

--- Problems/Medium/Python3/ContainerWithMostWater.py
+++ Problems/Medium/Python3/ContainerWithMostWater.py
@@ -1,5 +1,6 @@
 # Let's use the provided function to test with the new heights array and see if it finds the true maximum area.
-heights_test = [5,4,3,2,1,70,2,3,4,70]
+#heights_test = [3,4,7,5,7]
+heights_test = [8,5,8,4,3]
 
 # Re-using the previously defined function logic directly here for convenience
 def max_water_area(height):
@@ -8,6 +9,7 @@ def max_water_area(height):
     while left < right:
         current_area = min(height[left], height[right]) * (right - left)
         max_area = max(max_area, current_area)
+        print("max area", max_area," current area", current_area, " left", left, " right", right, " height left", height[left]," height right", height[right]," right - left", right - left)
         if height[left] < height[right]:
             left += 1
         else:
